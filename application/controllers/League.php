a6057abfec6e7c754efe9b62e803be66e83dadc2
KS <eodmalt@gmail.com>
2020-07-13 12:34:39 +0900

    200713 출근

--- application/controllers/League.php
+++ application/controllers/League.php
@@ -12,10 +12,22 @@ class League extends CI_Controller {
 	}
 	public function aevileague()
 	{
+		$tier_data = $this->league_model->getTierData();
+		$league_data = $this->league_model->getWorkingLeague();
+		$league_chartdata = $this->league_model->getLeagueChartData();
+		$league_userdata = $this->league_model->getLeagueUserData();
+		if ($league_data) {
+			$league_playdata = $this->league_model->getLeaguePlayInfo($league_data);
+		} else {
+			$league_playdata = null;
+		}
+
 		$arr_data = array(
-			"tier_data" => $this->league_model->getTierData(),
-			"league_data" => $this->league_model->getWorkingLeagueData(),
-			"league_user_data" => $this->league_model->getLeagueUserData(),
+			"tier_data" => $tier_data,
+			"league_data" => $league_data,
+			"league_chartdata" => $league_chartdata,
+			"league_userdata" => $league_userdata,
+			"league_playdata" => $league_playdata,
 		);
 		$this->load->view('league/aevileague', $arr_data);
     }
--- application/views/league/aevileague.php
+++ application/views/league/aevileague.php
@@ -13,8 +13,41 @@ require_once $common_dir . "/header.php";
 
       <!-- Page Heading -->
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"></h1>
+        <h1 class="h3 mb-0 text-gray-800">AEVILEAGUE</h1>
       </div>
+
+      <?php
+      if ($league_data) {
+      ?>
+      <div class="card border-primary mb-3" style="max-width:100%">
+        <div class="card-header">Header</div>
+        <div class="card-body">
+          <?php
+          if (count($league_chartdata) <= 3) {
+              foreach($league_chartdata as $chart_row) {
+          ?>
+              <img src="<?=$chart_row['lc_c_seq']?>">
+          <?php
+              }
+          } else {
+          ?>
+          <img src="">
+          <?php
+          }
+          ?>
+          <h4 class="card-title">Light card title</h4>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <?php
+      } else {
+      ?>
+      <div class="border border-secondary rounded p-3 mb-3">
+        진행중인 리그가 존재하지 않습니다.
+      </div>
+      <?php
+      }
+      ?>
 
     </div>
     <!-- /.container-fhd -->
