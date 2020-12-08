f70e061f3cde1bc80e13cfaa52bc0e859e782b40
LevartC <eodmalt@gmail.com>
2020-12-08 13:29:33 +0900

    201208 출근

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -4,14 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Ticket extends CI_Controller {
 	function __construct() {
 		parent::__construct();
-		$this->load->model('playinfo_model');
         $this->load->model('account_model');
         $this->load->model('ticket_model');
     }
 
 	public function index()
 	{
-		$this->load->view('ticket/ticketlist');
+		$this->load->view('ticket/studio');
 	}
 	public function total()
 	{
